ffeb1f0488f5ecf42d0b259a817e59b74aa1e6b7
m0nch1 <m0nch1.develop@gmail.com>
2021-01-07 13:59:31 +0900

    WIP: create layouts file

---
+++ src/pages/components/organisms/header.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className="z-10 border-b border-gray-200 bg-white sticky top-0">
+      <div className="flex items-center justify-between px-4 py-4 sm:px-6 lg:px-8">
+        <div className="md:flex items-center">
+          <Link href="/">
+            <a className="block">ここにLogo</a>
+          </Link>
+        </div>
+
+        <div className="flex text-sm">
+          <Link href="/blog">
+            <a className="font-medium text-gray-500 hover:text-gray-900">Blog</a>
+          </Link>
+          <Link href="/about">
+            <a className="ml-4 font-medium text-gray-500 hover:text-gray-900 sm:ml-12">About</a>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ src/pages/layouts/index.tsx
@@ -0,0 +1,3 @@
+export default function Layout({ children }: { children: JSX.Element }): JSX.Element {
+  return <div>{children}</div>;
+}
